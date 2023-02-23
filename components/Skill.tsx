60a4dd1f0395a7692ccc273b725619cdfe4a0a3f
Zach Xu <zach.popping@gmail.com>
2023-02-23 02:53:53 -0500

    fixed animation

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,27 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { motion } from 'framer-motion'
 
-type Props = {}
+type Props = {
+    isMobile: boolean,
+    isFromLeft: boolean
+}
 
 const Skill = (props: Props) => {
+
+    const { isMobile, isFromLeft } = props
+
     return (
-        <motion.div
-            initial={{
-                x: -300,
-                opacity: 0
-            }}
-            whileInView={{
-                x: 0,
-                opacity: 1
-            }}
-            transition={{
-                duration: 1.5
-            }}
-            className='relative group rounded-full overflow-hidden  '>
-            <img src="icons/javascript.svg" alt="" className='w-20 h-20' />
-            <div className='text-4xl absolute text-black font-bold hidden transition-opacity ease-in duration-500 opacity-0
-                group-hover:bg-gray-300 group-hover:inline-block w-20 h-20 top-0 left-0 text-center leading-[5rem] hover:opacity-90 cursor-default'>85%</div>
-        </motion.div>
+        <div className='relative group'>
+            <motion.img
+                initial={{
+                    x: isMobile ? 0 : isFromLeft ? -200 : 200,
+                    opacity: 0
+                }}
+                whileInView={{
+                    x: 0,
+                    opacity: 1
+                }}
+                transition={{
+                    duration: 1.5
+                }}
+                src="icons/javascript.svg" alt="" className='md:w-20 md:h-20 w-16 h-16 rounded-lg' >
+
+            </motion.img>
+            <div className='md:text-3xl text-2xl absolute text-black font-bold hidden transition-opacity ease-in duration-500 opacity-0
+                group-hover:bg-gray-300 group-hover:inline-block md:w-20 md:h-20 w-16 h-16  rounded-lg
+                 top-0 left-0 text-center hover:opacity-90 cursor-default'>
+                <div className='flex items-center justify-center h-full'>
+                    <p>85%</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,25 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Skill from './Skill'
 
 type Props = {}
 
 const Skills = (props: Props) => {
+
+    const [isMobile, setIsMobile] = useState<boolean>(false)
+
+    const skills = new Array(17).fill(0)
+
+    useEffect(() => {
+        console.log(window.innerWidth)
+        if (window.innerWidth <= 680) {
+            setIsMobile(true)
+        }
+    }, [isMobile])
+
     return (
         <div id='skills' className='h-screen flex flex-col relative items-center justify-center p-10 '>
-            <h3 className='uppercase absolute md:top-28 top-24 text-gray-400 text-xl tracking-[1rem]'>Skills</h3>
-            <h3 className='uppercase mb-20'>Hover over a skill for current proficiency</h3>
-            <div className='grid grid-cols-4 gap-5'>
-                <Skill />
-                <Skill />
-                <Skill />
-                <Skill />
-                <Skill />
-                <Skill />
-                <Skill />
-                <Skill />
-                <Skill />
-                <Skill />
-                <Skill />
+            <h3 className='uppercase absolute md:top-28 top-24 text-gray-400 text-xl tracking-[1rem] overflow-hidden'>Skills</h3>
+            <h3 className='uppercase my-10 bg-[rgb(36,36,36)] text-base text-gray-400'>Hover over a skill for current proficiency</h3>
+            <div className='grid grid-cols-4 gap-5 '>
+                {
+                    skills.map((skill, index) => (
+                        <Skill key={index} isMobile={isMobile} isFromLeft={index < skills.length / 2} />
+                    ))
+                }
+
             </div>
 
         </div>
